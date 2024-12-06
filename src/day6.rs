3abfe5f161eaeaafb494ec571bc057e6dd8f4595
Andreas Runfalk <andreas@runfalk.se>
2024-12-06 19:39:26 +0100

    Added solution for day 6.

---
+++ src/day6.rs
@@ -0,0 +1,113 @@
+use anyhow::{anyhow, Result};
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::{BufRead as _, BufReader};
+use std::path::Path;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum Direction {
+    Up,
+    Right,
+    Down,
+    Left,
+}
+
+impl Direction {
+    fn turn_right(&self) -> Self {
+        match self {
+            Self::Up => Self::Right,
+            Self::Right => Self::Down,
+            Self::Down => Self::Left,
+            Self::Left => Self::Up,
+        }
+    }
+
+    fn advance(&self, (x, y): (isize, isize)) -> (isize, isize) {
+        match self {
+            Self::Up => (x, y - 1),
+            Self::Right => (x + 1, y),
+            Self::Down => (x, y + 1),
+            Self::Left => (x - 1, y),
+        }
+    }
+}
+
+pub fn main(path: &Path) -> Result<(usize, Option<usize>)> {
+    let mut max_x = 0;
+    let mut max_y = 0;
+    let mut guard = None;
+    let mut map = HashSet::new();
+
+    let file = File::open(path)?;
+    for (y, line) in BufReader::new(file).lines().enumerate() {
+        for (x, c) in line?.chars().enumerate() {
+            match c {
+                '#' => {
+                    map.insert((x as isize, y as isize));
+                }
+                '^' => guard = Some((x as isize, y as isize)),
+                _ => {}
+            }
+            max_x = max_x.max(x as isize);
+        }
+        max_y = max_y.max(y as isize);
+    }
+
+    let guard = guard.ok_or_else(|| anyhow!("No guard start location found"))?;
+    let x_bounds = 0..max_x;
+    let y_bounds = 0..max_y;
+
+    // Find the path of the guard
+    let mut visited = HashSet::new();
+    let mut visited_with_direction = HashSet::new();
+    let (mut x, mut y) = guard;
+    let mut dir = Direction::Up;
+    while x_bounds.contains(&x) && y_bounds.contains(&y) {
+        visited.insert((x, y));
+        if !visited_with_direction.insert((dir, x, y)) {
+            return Err(anyhow!("Loop found without altering the map"));
+        }
+        let (nx, ny) = dir.advance((x, y));
+        if map.contains(&(nx, ny)) {
+            dir = dir.turn_right();
+            continue;
+        }
+        (x, y) = (nx, ny);
+    }
+
+    let visited_positions = visited.len();
+
+    // Use the original path to determine where we should try to inject obstacles
+    let mut num_possible_obstacle_positions = 0;
+    let obstacles_to_try: HashSet<_> = visited_with_direction
+        .into_iter()
+        .map(|(dir, x, y)| dir.advance((x, y)))
+        .filter(|(x, y)| x_bounds.contains(x) && y_bounds.contains(y))
+        .collect();
+    for (ox, oy) in obstacles_to_try {
+        let mut dir = Direction::Up;
+        let (mut x, mut y) = guard;
+
+        let mut visited = HashSet::new();
+        while x_bounds.contains(&x) && y_bounds.contains(&y) {
+            if !visited.insert((dir, x, y)) {
+                num_possible_obstacle_positions += 1;
+                break;
+            }
+            let (nx, ny) = dir.advance((x, y));
+            if (nx == ox && ny == oy) || map.contains(&(nx, ny)) {
+                dir = dir.turn_right();
+                continue;
+            }
+            (x, y) = (nx, ny);
+        }
+    }
+
+    Ok((visited_positions, Some(num_possible_obstacle_positions)))
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    test_real_input!(6, 5086, 1770);
+}
--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day6;
 
 #[derive(Debug, Parser)]
 struct Options {
@@ -44,6 +45,7 @@ fn main() -> Result<()> {
         2 => as_result(day2::main(&input)?),
         3 => as_result(day3::main(&input)?),
         4 => as_result(day4::main(&input)?),
+        6 => as_result(day6::main(&input)?),
         1..=25 => return Err(anyhow!("No implementation for this day yet")),
         day => return Err(anyhow!("Day {} is not a valid day for advent of code", day)),
     };
